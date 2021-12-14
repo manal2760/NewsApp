52e00adcf42bb53b829d58214750f8251db39205
manal <manal.stihi69@gmail.com>
2021-12-14 21:02:36 +0100

    First News App

---
+++ app/src/main/java/ma/ensaf/newsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package ma.ensaf.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//912a86cdf5b04b28a8b30878886c422b
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
