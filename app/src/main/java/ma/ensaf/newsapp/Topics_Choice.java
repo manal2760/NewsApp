e1878b3e81fa2c7dc9dfafc5a4e59079129539a2
simipa <yasmine.aourabi@gmail.com>
2021-12-15 16:26:38 +0100

    .

--- app/src/main/java/ma/ensaf/newsapp/Topics_Choice.java
+++ app/src/main/java/ma/ensaf/newsapp/Topics_Choice.java
@@ -1,14 +1,32 @@
 package ma.ensaf.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Topics_Choice extends AppCompatActivity {
+    AppCompatButton science, education, business,divertissement,santé,politique,sports,technologie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topics_choice);
+        science = (AppCompatButton) findViewById(R.id.science);
+        education = (AppCompatButton) findViewById(R.id.education);
+        business=(AppCompatButton) findViewById(R.id.business);
+        divertissement=(AppCompatButton) findViewById(R.id.divertissement);
+        santé=(AppCompatButton) findViewById(R.id.santé);
+        politique=(AppCompatButton) findViewById(R.id.politique);
+        sports=(AppCompatButton) findViewById(R.id.sport);
+        technologie=(AppCompatButton) findViewById(R.id.tech);
+        science.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                science.setBackgroundResource(R.drawable.onclick_pill_button);
+
+            }
+        });
     }
 }
