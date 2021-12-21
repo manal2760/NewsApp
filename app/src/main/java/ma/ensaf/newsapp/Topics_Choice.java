9032b79594b79820d83b39415aca0b83d38a882a
manal <manal.stihi69@gmail.com>
2021-12-21 11:21:38 +0100

    add categories to firebase

--- app/src/main/java/ma/ensaf/newsapp/Topics_Choice.java
+++ app/src/main/java/ma/ensaf/newsapp/Topics_Choice.java
@@ -6,18 +6,32 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Topics_Choice extends AppCompatActivity {
     AppCompatButton science, education, business,divertissement,santé,politique,sports,technologie;
-    List<AppCompatButton> categories = new ArrayList<>();
-    Boolean isClicked=false;
+    //List<AppCompatButton> categories = new ArrayList<>();
+    DatabaseReference reference;
+    FirebaseAuth mAuth;
+    Categories categories= new Categories();
+    String text1="science";
+    String text2="education";
+    String text3="business";
+    String text4="politique";
+    String text5="sports";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topics_choice);
+        reference= FirebaseDatabase.getInstance().getReference().child("categories");
         science = (AppCompatButton) findViewById(R.id.science);
         education = (AppCompatButton) findViewById(R.id.education);
         business=(AppCompatButton) findViewById(R.id.business);
@@ -30,48 +44,96 @@ public class Topics_Choice extends AppCompatActivity {
         science.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isClicked==false){
+                Boolean isClicked1=false;
+
+                if (isClicked1==false){
                     click(science);
+                    categories.setText(text1);
+                    reference.push().setValue(categories);
                 }
 
                 else{
                     unclick(science);
-                    categories.remove(science);
+                    reference.child(text1).removeValue();
 
                 }
-                isClicked=!isClicked;
+                isClicked1=!isClicked1;
             }
         });
 
         education.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                education.setBackgroundResource(R.drawable.onclick_pill_button);
-                education.setTextColor(getResources().getColor(R.color.dark1));
+                Boolean isClicked2=false;
+                if (isClicked2==false){
+                    click(science);
+                    categories.setText(text2);
+                    reference.push().setValue(categories);
+                }
+
+                else{
+                    unclick(science);
+                    reference.child(text2).removeValue();
+
+                }
+                isClicked2=!isClicked2;
 
             }
         });
         business.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                business.setBackgroundResource(R.drawable.onclick_pill_button);
-                business.setTextColor(getResources().getColor(R.color.dark1));
+                Boolean isClicked3=false;
+                if (isClicked3==false){
+                    click(science);
+                    categories.setText(text3);
+                    reference.push().setValue(categories);
+                }
+
+                else{
+                    unclick(science);
+                    reference.child(text3).removeValue();
+
+                }
+                isClicked3=!isClicked3;
 
             }
         });
         politique.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                politique.setBackgroundResource(R.drawable.onclick_pill_button);
-                politique.setTextColor(getResources().getColor(R.color.dark1));
+                Boolean isClicked4=false;
+                if (isClicked4==false){
+                    click(science);
+                    categories.setText(text4);
+                    reference.push().setValue(categories);
+                }
+
+                else{
+                    unclick(science);
+                    reference.child(text4).removeValue();
+
+                }
+                isClicked4=!isClicked4;
 
             }
         });
         sports.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sports.setBackgroundResource(R.drawable.onclick_pill_button);
-                sports.setTextColor(getResources().getColor(R.color.dark1));
+                Boolean isClicked5=false;
+                if (isClicked5==false){
+                    click(science);
+                    categories.setText(text5);
+                    reference.push().setValue(categories);
+                }
+
+                else{
+                    unclick(science);
+                    reference.child(text5).removeValue();
+
+                }
+                isClicked5=!isClicked5;
 
             }
         });
