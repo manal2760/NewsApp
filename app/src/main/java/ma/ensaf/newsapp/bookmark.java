4db46cae7985240e5d982d38a376c8a28bb29851
simipa <yasmine.aourabi@gmail.com>
2021-12-22 19:21:51 +0100

    .

---
+++ app/src/main/java/ma/ensaf/newsapp/bookmark.java
@@ -0,0 +1,14 @@
+package ma.ensaf.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bookmark extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookmark);
+    }
+}
