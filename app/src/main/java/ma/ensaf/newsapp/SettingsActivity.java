51eb7b2104fce6a4a526ea9574697f7f3ffed5ab
simipa <yasmine.aourabi@gmail.com>
2022-01-17 18:20:19 +0100

    edit profile picture

--- app/src/main/java/ma/ensaf/newsapp/SettingsActivity.java
+++ app/src/main/java/ma/ensaf/newsapp/SettingsActivity.java
@@ -2,9 +2,13 @@ package ma.ensaf.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -13,15 +17,24 @@ import com.google.firebase.auth.FirebaseAuth;
 public class SettingsActivity extends AppCompatActivity {
     private  LinearLayout lin1;
     private RelativeLayout rel1;
+    ImageView ProfilePicture;
     FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        ProfilePicture= findViewById(R.id.profilepicture);
         lin1= (LinearLayout) findViewById(R.id.linId);
         rel1= (RelativeLayout) findViewById(R.id.relaId);
         mAuth= FirebaseAuth.getInstance();
+        ProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+
+            }
+        });
         rel1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,5 +50,15 @@ public class SettingsActivity extends AppCompatActivity {
                 startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri imageuri = data.getData();
+                ProfilePicture.setImageURI(imageuri);
+            }
+        }
     }
 }
